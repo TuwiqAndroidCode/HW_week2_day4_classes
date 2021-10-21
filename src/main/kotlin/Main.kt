ab924b4c205d1213798c38dfa9b5faa1f6f4bdd9
faisal <faisal.Nahi.ruh.k@tuwaiq.edu.sa>
2021-10-21 09:17:30 +0300

    solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,39 @@
 fun main(args: Array<String>) {
 
-    // write your code here
+    fun main() {
+
+
+
+
+
+
+    }
+
+    class Course {
+        var courseName:String= "Arabic"
+        var numberOflectures:Int=10
+        val students:String = "Faisal,ALi,Sam"
+        val professor:String = "Zamel"
+
+    }
+
+    class Professor {
+        var firstName:String="Zamel"
+        var lastName:String="Mohammed"
+        val courses11 :String= " English , Math "
+        // val telephone:String
+        //   get() {
+        //     dTODO()
+        //  }
+
+        class Student {
+            var firstName :String="Faisal"
+            var lastName:String="Nahi "
+            val courses:String="English, Math"
+
+        }
+
+    }
 
 
 }
